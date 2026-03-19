fd035932f9007b381ddd039ec0f8e38562d3b541
Gateway <mironpixel47@gmail.com>
2026-03-20 00:27:14 +0300

    Added the ability to add a configuration file to automatically fill in input fields.

--- Fan-script.py
+++ Fan-script.py
@@ -2,8 +2,36 @@ import os
 import time
 import ipaddress
 import getpass
-import os
 import subprocess
+import json
+
+def auth_file():
+    default_config = "data.json"
+    folder = input("Enter folder name (leave empty for current): ").strip()
+    filename = input(f"Enter filename [default - {default_config}]:  ").strip() or "data"
+    if not filename.endswith(".json"):
+        filename += ".json"
+    path = os.path.join(folder, filename)
+    if os.path.exists(path):
+        confirm = input(f"File '{filename}' already exists. Overwrite? (y/n): ").lower().strip()
+        if confirm != 'y':
+            print("Saving cancelled.")
+            return 
+    data = {
+        "ip": input("Server IP: ").strip(),
+        "user": input("Username: ").strip(),
+        "passwd": input("Password: ").strip()
+    }
+
+    try:
+        if folder:
+            os.makedirs(folder, exist_ok=True)
+        with open(path, "w", encoding="utf-8") as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
+        print(f"File successfully saved at: {os.path.abspath(path)}")
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return
 
 def clear_screen():
     os.system('clear')
@@ -27,10 +55,31 @@ def check_ipmitool():
         return False
 
 def fan_speed():
-        ip = input("Server IP: ")
-        valid_ip = ipaddress.ip_address(ip)
-        user = input("Username: ")
-        passwd = getpass.getpass("Password: ")
+    default_config = "data.json"
+    user_path = input(f"Enter the path to the JSON file [default - {default_config}]: ").strip()
+    config_file = user_path if user_path else default_config
+    ip, user, passwd = None, None, None
+    if os.path.exists(config_file):
+        try:
+            with open(config_file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                ip = data.get('ip')
+                user = data.get('user')
+                passwd = data.get('passwd')
+                print(f"Data loaded from: {config_file}")
+        except Exception as e:
+            print(f"Error reading file: {e}")
+
+    if not all([ip, user, passwd]):
+        if os.path.exists(config_file):
+            print("The file was found, but some data is missing. Please fill it in:")
+        else:
+            print(f"File '{config_file}' not found. Please enter the information manually:")
+        ip = ip or input("Server IP: ")
+        user = user or input("Username: ")
+        passwd = passwd or getpass.getpass("Password: ")
+    try:
+        ipaddress.ip_address(ip)
         number = int(input("Enter a number from 0-100 to set the speed: "))
         assert 0 <= number <= 100, f"The number {number} is out of range!"
         hexn = hex(number)
@@ -39,9 +88,11 @@ def fan_speed():
         if sure in {'y', 'Y'} or sure.lower() == 'y' or sure.upper() == 'Y':
             print(os.system(f"ipmitool -I lanplus -H {ip} -U {user} -P {passwd} raw 0x30 0x30 0x01 0x00"))
             print(os.system(f"ipmitool -I lanplus -H {ip} -U {user} -P {passwd} raw 0x30 0x30 0x02 0xff {hexn}"))
-            print("Done\n ")
+            print("Done\n")
         else:
-            print("Exit to menu\n ")
+            print("Cancelled\n")
+    except Exception as e:
+        print(f"An unexpected error occurred {e}")
 
 def fan_control():
     choice_input = input("0 - Manual mode\n1 - Dell auto mode\n2 - Return to menu\nEnter a number: ").strip()
@@ -50,10 +101,28 @@ def fan_control():
         return
     number = int(choice_input)
     assert 0 <= number <= 1, f"The number {number} is out of range!"
-    ip = input("Server IP: ")
-    valid_ip = ipaddress.ip_address(ip)
-    user = input("Username: ")
-    passwd = getpass.getpass("Password: ")
+    default_config = "data.json"
+    user_path = input(f"Enter path to JSON file [default: {default_config}]: ").strip()
+    config_file = user_path if user_path else default_config
+    ip, user, passwd = None, None, None
+    if os.path.exists(config_file):
+        try:
+            with open(config_file, 'r', encoding='utf-8') as f:
+                data = json.load(f)
+                ip = data.get('ip')
+                user = data.get('user')
+                passwd = data.get('passwd')
+                print(f"Data loaded from: {config_file}")
+        except Exception as e:
+            print(f"Error reading file: {e}")
+    if not all([ip, user, passwd]):
+        if os.path.exists(config_file):
+            print("The file was found, but some data is missing. Please fill it in:")
+        else:
+            print(f"File '{config_file}' not found. Please enter the information manually:")
+        ip = ip or input("Server IP: ")
+        user = user or input("Username: ")
+        passwd = passwd or getpass.getpass("Password: ")
     print(f"Your settings:\n-------------------------\nIP: {ip}\nUsername: {user}\nPassword: {passwd}\n-------------------------\n")
     sure = input("Are you sure. (y/n)").strip()
     if sure in {'y', 'Y'} or sure.lower() == 'y' or sure.upper() == 'Y':
@@ -62,9 +131,7 @@ def fan_control():
             print("Done\n ")
         elif number == 1:
             print(os.system(f"ipmitool -I lanplus -H {ip} -U {user} -P {passwd} raw 0x30 0x30 0x01 0x01"))
-            sure = input("Done.\n ")
-        else:
-            print("Exit to menu\n ")
+            print("Done.\n ")
     else:
         print("Exit to menu\n ")
 
@@ -83,7 +150,8 @@ def print_gradient_logo(text):
 
 commands = {
         0: fan_speed,
-        1: fan_control
+        1: fan_control,
+        2: auth_file
     }
 
 LOGO = (r"""
@@ -101,7 +169,7 @@ $$ |  \$$$$$$$ |$$ |  $$ |      $$$$$$$  |\$$$$$$$\ $$ |      $$ |$$$$$$$  | \$$
     """)
 def main():
     print("\033[1;31mWARNING: Use at your own risk. This software allows you to manually control the server fan speed.\033[0m\n-------------------------")
-    choice = input("0 - Fan speed control\n1 - Fan mode control\nExit - Quit from script\n-------------------------\nEnter item number/word: ")
+    choice = input("0 - Fan speed control\n1 - Fan mode control\n2 - Add or edit IPMI auth file\nExit - Quit from script\n-------------------------\nEnter item number/word: ")
     if choice.lower() == 'exit':
         print("Exiting the fan-script")
         time.sleep(1)
